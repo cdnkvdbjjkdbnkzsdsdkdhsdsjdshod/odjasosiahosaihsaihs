2efcaae85fc563e090efd158a932373fcbea8e31
cdnkvdbjjkdbnkzsdsdkdhsdsjdshod <41491967+cdnkvdbjjkdbnkzsdsdkdhsdsjdshod@users.noreply.github.com>
2018-07-22 07:50:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,6 @@ const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '!';
-client.login(discord_token);
 client.on('ready', function() {
     console.log(`i am ready ${client.user.username}`);
 });
